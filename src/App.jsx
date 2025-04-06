cfb758add0afcf9641fb7c71d002130fbe089936
kisuk <cks71714@naver.com>
2025-04-06 23:00:29 +0900

    feat: 헤더, 카테고리 구현 및 카테고리 mock 데이터 추가
    - 카테고리별 mock 데이터 추가
    - 헤더 수정
    - Layout 수정
    
    FDORA-23

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,27 @@ import LiveSetup from './pages/live/LiveSetup';
 import StreamView from './pages/live/StreamView';
 import Event from './pages/event/Event';
 import ChatSupport from './pages/support/ChatSupport';
+import Category from './pages/category/Category';
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/join" element={<Join/>} />
-          <Route path="/wishlist" element={<Wishlist/>} />
-          <Route path="/mypage" element={<Mypage/>} />
-          <Route path="/cart" element={<Cart/>} />
-          <Route path="/event" element={<Event/>} />
-          <Route path="/support" element={<ChatSupport/>} />
-          <Route path="/live" element={<Live/>} /> {/* 방송리스트화면 */}
-          <Route path="/live/setup" element={<LiveSetup/>} /> {/* 방송준비화면 */}
-          <Route path="/live/view" element={<StreamView/>} /> {/* 방송 시청하는 화면 ex)/live/view/123 */} 
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/join" element={<Join/>} />
+            <Route path="/wishlist" element={<Wishlist/>} />
+            <Route path="/mypage" element={<Mypage/>} />
+            <Route path="/cart" element={<Cart/>} />
+            <Route path="/category" element={<Category />} />
+            <Route path="/event" element={<Event/>} />
+            <Route path="/support" element={<ChatSupport/>} />
+            <Route path="/live" element={<Live/>} /> {/* 방송리스트화면 */}
+            <Route path="/live/setup" element={<LiveSetup/>} /> {/* 방송준비화면 */}
+            <Route path="/live/view" element={<StreamView/>} /> {/* 방송 시청하는 화면 ex)/live/view?id=123 */}
+          </Route>
         </Routes>
-      </Layout>
     </BrowserRouter>
   );
 }
