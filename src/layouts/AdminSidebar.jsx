3d5a4fc7c4ac28395e1dea553ef46618849ac162
Bhin <bbhinhb@gmail.com>
2025-04-08 23:12:37 +0900

    feat: home 버튼 효과 제거
    - 판매자, 관리자 홈버튼 hover 효과 제거
    
    FDORA-9

--- src/layouts/AdminSidebar.jsx
+++ src/layouts/AdminSidebar.jsx
@@ -8,7 +8,7 @@ function AdminSidebar() {
     <div className="flex flex-col w-[250px] h-screen p-4 bg-brown max-w-[250px]">
       
       <nav className="flex flex-col gap-1">
-      <Link to="/admin" className="text-left text-gray-light text-lg font-bold px-4 py-2 rounded-md hover:bg-[#5A514F] transition">H O M E</Link>
+      <Link to="/admin" className="text-left text-gray-light text-lg font-bold px-4 py-2 transition">H O M E</Link>
       </nav>
 
       <div className="border-t-2 border-gray-light my-4" />
